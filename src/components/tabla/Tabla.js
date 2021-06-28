e2afac5e02e64016a8f16ee7252413752918fa91
jxx0911 <kevin.322.vr@gmail.com>
2021-06-28 16:58:49 -0500

    Validar lote en proceso, se logro captar el inpuChange del formulario en la tabla

--- src/components/tabla/Tabla.js
+++ src/components/tabla/Tabla.js
@@ -1,15 +1,43 @@
-import React from "react";
-import "../../App.css";
-import MaterialTable from "material-table";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useEffect, useState } from "react";
 import { Excel } from "../../helpers/Excel";
 import { useSedes } from "../../hooks/useSedes";
 import { useServicios } from "../../hooks/useServicios";
+import { helpHttp } from "../../helpers/helpHttp";
+import axios from "axios";
+
+import "../../App.css";
+import MaterialTable from "material-table";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const initialForm = {
+	cod_servicio: "",
+	nombre_sede: "",
+	fecha_salida: "",
+};
 
 function Tabla() {
+	let api = helpHttp();
 	const { importExcel, data, colDefs } = Excel();
 	const { sedes } = useSedes();
 	const { servicios } = useServicios();
+	/* const [loading, setLoading] = useState(false); */
+	const [form, setForm] = useState(initialForm);
+
+	const handleInputChange = (e) => {
+		setForm({
+			...form,
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	console.log(form);
+
+	/* const validarLote = async () => {
+		await axios
+			.get(
+				`http://167.99.115.105/bdmarsa/tercera/lote/validar?cod_servicio=${}&nombre_sede=${}&fecha_salida=${}`
+			)
+	} */
 
 	return (
 		<>
@@ -20,7 +48,13 @@ function Tabla() {
 					{/* Select para Sede */}
 					<div className="sede">
 						<div className="form-label">SEDE</div>
-						<select className="form-select">
+						<select
+							name="nombre_sede"
+							className="form-select"
+							onChange={handleInputChange}
+							value={form.nombre_sede}
+						>
+							<option defaultValue> -- SELECCIONE SEDE -- </option>
 							{sedes.map((item, index) => (
 								<option key={index} value={item.sede}>
 									{item.sede}
@@ -31,9 +65,15 @@ function Tabla() {
 					{/* Select para Servicio */}
 					<div className="servicio">
 						<div className="form-label">SERVICIO</div>
-						<select className="form-select">
+						<select
+							className="form-select"
+							onChange={handleInputChange}
+							value={form.cod_servicio}
+							name="cod_servicio"
+						>
+							<option defaultValue> -- SELECCIONE SERVICIO -- </option>
 							{servicios.map((item, index) => (
-								<option key={index} value={item.nombre_servicio}>
+								<option key={index} value={item.id_servicio}>
 									{item.nombre_servicio}
 								</option>
 							))}
@@ -45,16 +85,19 @@ function Tabla() {
 						<input
 							type="date"
 							className="form-control"
-							name="example-text-input"
+							name="fecha_salida"
 							placeholder="Input placeholder"
+							onChange={handleInputChange}
 						/>
 					</div>
 				</div>
 				<div className="container mt-1">
 					<div className="botones">
-						<button className="btn btn-primary generar">Generar Lote</button>
-						<button className="btn btn-danger limpiar">Limpiar</button>
-						<button className="btn btn-success registrar">Registrar</button>
+						<button
+							className="btn btn-primary generar" /* onClick={validarLote} */
+						>
+							Generar Lote
+						</button>
 					</div>
 					<input className="col-3" type="file" onChange={importExcel} />
 				</div>
@@ -62,6 +105,7 @@ function Tabla() {
 				<div className="table-responsive">
 					<MaterialTable title="Marsa Data" data={data} columns={colDefs} />
 				</div>
+				<button className="btn btn-success registrar">Registrar</button>
 			</div>
 		</>
 	);
