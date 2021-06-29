bd788853bef75fa56b547c571c6ad2c959a37911
Kevin Villanueva <kevin.322.vr@gmail.com>
2021-06-28 23:31:18 -0500

    Se probara el registro de Lotes, falta arreglar el tema de la fecha para validar Lote

--- src/components/tabla/Tabla.js
+++ src/components/tabla/Tabla.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Excel } from "../../helpers/Excel";
 import { useSedes } from "../../hooks/useSedes";
 import { useServicios } from "../../hooks/useServicios";
-import { helpHttp } from "../../helpers/helpHttp";
+/* import { helpHttp } from "../../helpers/helpHttp"; */
 import axios from "axios";
 
 import "../../App.css";
@@ -15,11 +15,23 @@ const initialForm = {
 	fecha_salida: "",
 };
 
+const initialLote = {
+	cod_servicio: "",
+	nombre_servicio: "",
+	cod_sede: "",
+	nombre_sede: "",
+	fecha_actual: "", // toLocalDateString()
+	hora: "", // toLocalTimeString()
+	fecha_salida: "", // form.fecha_salida
+};
+
 function Tabla() {
-	let api = helpHttp();
+	/* let api = helpHttp(); */
 	const { importExcel, data, colDefs } = Excel();
 	const { sedes } = useSedes();
+	const [sedeLote, setSedeLote] = useState();
 	const { servicios } = useServicios();
+	const [servicioLote, setServicioLote] = useState();
 	/* const [loading, setLoading] = useState(false); */
 	const [form, setForm] = useState(initialForm);
 
@@ -30,23 +42,29 @@ function Tabla() {
 		});
 	};
 
-	console.log(form);
+	/* 
+	console.log(form.fecha_salida);
+	let fecha = form.fecha_salida.toLocaleDateString("en-US");
+	console.log(fecha); */
 
-	/* const validarLote = async () => {
+	const validarLote = async () => {
 		await axios
 			.get(
-				`http://167.99.115.105/bdmarsa/tercera/lote/validar?cod_servicio=${}&nombre_sede=${}&fecha_salida=${}`
+				`http://167.99.115.105/bdmarsa/tercera/lote/validar?cod_servicio=${form.cod_servicio}&nombre_sede=${form.nombre_sede}&fecha_salida=06/19/2021`
 			)
-	} */
+			.then((response) => {
+				console.log(response);
+			});
+	};
 
 	return (
 		<>
 			<div className="App">
 				<h1 align="center">TABLE IMPORT</h1>
 				{/* <h4 align="center">Descripcion</h4> */}
-				<div className="container table-inputs">
+				<div className="container">
 					{/* Select para Sede */}
-					<div className="sede">
+					<div>
 						<div className="form-label">SEDE</div>
 						<select
 							name="nombre_sede"
@@ -63,7 +81,7 @@ function Tabla() {
 						</select>
 					</div>
 					{/* Select para Servicio */}
-					<div className="servicio">
+					<div>
 						<div className="form-label">SERVICIO</div>
 						<select
 							className="form-select"
@@ -80,32 +98,29 @@ function Tabla() {
 						</select>
 					</div>
 					{/* Input para fecha */}
-					<div className="fecha">
-						<label className="form-label">FECHA DE ATENCION</label>
-						<input
-							type="date"
-							className="form-control"
-							name="fecha_salida"
-							placeholder="Input placeholder"
-							onChange={handleInputChange}
-						/>
-					</div>
-				</div>
-				<div className="container mt-1">
-					<div className="botones">
-						<button
-							className="btn btn-primary generar" /* onClick={validarLote} */
-						>
+					<div>
+						<div>
+							<label className="form-label">FECHA DE ATENCION</label>
+							<input
+								type="date"
+								className="form-control"
+								name="fecha_salida"
+								placeholder="Input placeholder"
+								onChange={handleInputChange}
+							/>
+						</div>
+						<button className="btn btn-dark" onClick={validarLote}>
 							Generar Lote
 						</button>
+						<button className="btn btn-primary">Registrar Lote</button>
+						<input className="col-3" type="file" onChange={importExcel} />
 					</div>
-					<input className="col-3" type="file" onChange={importExcel} />
 				</div>
 
 				<div className="table-responsive">
 					<MaterialTable title="Marsa Data" data={data} columns={colDefs} />
 				</div>
-				<button className="btn btn-success registrar">Registrar</button>
+				<button className="btn btn-success">Registrar</button>
 			</div>
 		</>
 	);
