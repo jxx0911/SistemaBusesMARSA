e9163fffb486d06a9269c52189444b167ceed170
jxx0911 <kevin.322.vr@gmail.com>
2021-07-02 14:27:08 -0500

    Se hara Manifiesto

--- src/components/FormSteps/Inmune.js
+++ src/components/FormSteps/Inmune.js
@@ -1,10 +1,17 @@
 import React from "react";
+import axios from "axios";
+
+let form = {};
+let time = new Date();
 
 export const Inmune = ({ paciente, bus }) => {
 	function imprimirElemento(elemento) {
 		let ventana = window.open("", "PRINT", "height=400,width=600");
-		ventana.document.write("<html><head><title>" + document.title + "</title>");
-		ventana.document.write('<link rel="stylesheet" href="./ticket.css">');
+		ventana.document.write("<html><><title>" + document.title + "</title>");
+		ventana.document.write('<link rel="stylesheet" href="style.css">');
+		ventana.document.write(
+			"<style>*{margin: 0;padding: 0;border: 0;}@page{margin: 0;}body{ font-family: Lucida Sans Typewriter; }.size18{font-size: 18px;font-weight: bold;text-align:center;}.size14{font-size: 14px;text-align:center;}.size14izq{font-size: 14px;font-weight: bold;}.size14der{font-size: 14px;text-align:right;}.centrar{text-align:center;}</style>"
+		);
 		ventana.document.write("</head><body >");
 		ventana.document.write(elemento.innerHTML);
 		ventana.document.write("</body></html>");
@@ -17,26 +24,62 @@ export const Inmune = ({ paciente, bus }) => {
 		return true;
 	}
 
+	const registroManifiesto = async (e) => {
+		e.preventDefault();
+		form = {
+			clave: paciente.clave,
+			fecha_act: "2021-07-01",
+			hora_act: time.toLocaleTimeString(),
+			placa: bus.placa,
+			capacidad_aforo: bus.capacidad,
+		};
+		console.log(form);
+		const resp = await axios.post(
+			"http://167.99.115.105/bdmarsa/tercera/ticket/ticketManifiesto",
+			form
+		);
+		console.log(resp);
+	};
+
 	const imprimir = () => {
 		let div = document.querySelector("#imprimible");
 		imprimirElemento(div);
 	};
 
 	return (
-		<div id="imprimible">
-			<h3>TICKET : {paciente.status}</h3>
-			<p>
-				D.N.I. : {paciente.dni} <br />
-				Resultado P1 : {paciente.resultado} <br />
-				Sintomatologia : {paciente.sintomatologia} <br />
-				Fecha de Prueba : {paciente.fecha_examen} <br />
-				Clinica : {paciente.clinica} <br />
+		<div>
+			<div id="imprimible">
+				<p className="size18">CONTROL DE SALIDA</p>
+				<p className="size14">Minera Aurífera Retamas S.A.</p>
+				<p className="size14">20132367800</p>
+				<p className="centrar">--------------------------------------</p>
+				<p className="size18">{paciente.status}</p>
+				<p className="centrar">--------------------------------------</p>
+				<p className="size14izq">
+					Dni:<span>{paciente.dni}</span>
+				</p>
+				<p className="size14izq">Resultado P1: {paciente.resultado}</p>
+				<p className="size14izq">Fecha Prueba: {paciente.fecha_examen}</p>
+				<p className="size14izq">Sintomatología: {paciente.sintomatologia}</p>
+				<p className="size14izq">Clinica: {paciente.clinica}</p>
 				<br />
-				Bus : {bus.empresa} <br />
-				Placa : {bus.placa} <br />
-				Nº de Asiento : <br />
-				Mensaje : {bus.mensaje}
-			</p>
+				<p className="centrar">--------------------------------------</p>
+				<p className="size18">INFORMACION BUS</p>
+				<p className="centrar">--------------------------------------</p>
+				<br />
+				<p className="size14izq">
+					PLACA:<span>{bus.placa}</span>
+					<br />
+					EMPRESA:<span>{bus.empresa}</span>
+				</p>
+				<br />
+				<p className="centrar">--------------------------------------</p>
+				<p className="centrar">{bus.mensaje}</p>
+				<p className="centrar">--------------------------------------</p>
+			</div>
+			<button onClick={registroManifiesto} className="btn btn-primary">
+				Registrar MANIFIESTO
+			</button>
 			<button onClick={imprimir} className="btn btn-success">
 				Imprimir
 			</button>
