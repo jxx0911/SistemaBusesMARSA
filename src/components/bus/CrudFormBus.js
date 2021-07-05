18534441feb02d5702945f8a570358a307e88480
jxx0911 <kevin.322.vr@gmail.com>
2021-07-05 13:55:45 -0500

    Estilisando Bus.js, se reemplazo el select por un datalist para input empresas

--- src/components/bus/CrudFormBus.js
+++ src/components/bus/CrudFormBus.js
@@ -58,19 +58,34 @@ const CrudFormBus = ({
 
 	return (
 		<div>
-			<h3>{dataToEdit ? "EDITAR" : "AGREGAR"}</h3>
 			<form onSubmit={handleSubmit}>
-				<div className="mb-3 d-flex">
+				<div className="container">
+					<h3>{dataToEdit ? "EDITAR" : "AGREGAR"}</h3>
 					<input
 						type="text"
-						className="form-control"
+						className="form-control mb-3"
 						name="placa"
 						placeholder="Placa"
 						onChange={handleChange}
 						value={form.placa}
 					/>
-				</div>
-				<select
+					<input
+						type="text"
+						className="form-control mb-3"
+						placeholder="Empresa"
+						onChange={handleChange}
+						value={form.empresa}
+						name="empresa"
+						list="empresas"
+					/>
+					<datalist id="empresas">
+						{empresasBus.map((item, index) => (
+							<option key={index} value={item.nombre_empresa}>
+								{item.nombre_empresa}
+							</option>
+						))}
+					</datalist>
+					{/* <select
 					name="empresa"
 					className="form-select"
 					value={form.empresa}
@@ -87,23 +102,26 @@ const CrudFormBus = ({
 							</option>
 						)
 					)}
-				</select>
-				<input
-					type="number"
-					name="capacidad"
-					placeholder="Capacidad"
-					onChange={handleChange}
-					value={form.capacidad}
-				/>
-				<input
-					type="text"
-					name="chofer"
-					placeholder="Conductor"
-					onChange={handleChange}
-					value={form.chofer}
-				/>
-				<input type="submit" value="Enviar" />
-				<input type="reset" value="Limpiar" onClick={handleReset} />
+				</select> */}
+					<input
+						className="form-control mb-3"
+						type="number"
+						name="capacidad"
+						placeholder="Capacidad"
+						onChange={handleChange}
+						value={form.capacidad}
+					/>
+					<input
+						className="form-control mb-3"
+						type="text"
+						name="chofer"
+						placeholder="Conductor"
+						onChange={handleChange}
+						value={form.chofer}
+					/>
+					<input type="submit" value="Enviar" />
+					<input type="reset" value="Limpiar" onClick={handleReset} />
+				</div>
 			</form>
 		</div>
 	);
