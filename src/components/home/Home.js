ff75fd3be823f4a302cb194b5db151ed86f77d2c
Kevin Villanueva <kevin.322.vr@gmail.com>
2021-07-06 23:29:16 -0500

    Vista para registro de paciente en Home

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,39 @@
 import React from "react";
+import { ImSearch } from "react-icons/im";
 
 export const Home = () => {
-	return <div>Home</div>;
+	return (
+		<>
+			<div className="container-tight py-2">
+				<div className="card card-md">
+					<div className="card-body text-center py-4 p-sm-5">
+						<h2>REGISTRO DE PACIENTE</h2>
+					</div>
+					<div className="card-body">
+						<fieldset className="form-fieldset">
+							<div className="mb-3">
+								<div className="d-flex flex-row">
+									<input
+										type="number"
+										className="form-control"
+										placeholder="Ingrese D.N.I."
+										/* value={form.nro_documento}
+										onChange={handleInputChange} */
+										name="nro_documento"
+										/* onKeyPress={handleKeyPress} */
+									/>
+									<button
+										className="btn btn-dark m-2"
+										/* onClick={importarPaciente} */
+									>
+										<ImSearch />
+									</button>
+								</div>
+							</div>
+						</fieldset>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
